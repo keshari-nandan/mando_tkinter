73a2194bcffde9b0ea0cd8af755633f9ed0e426f
Keshari Nandan <keshari.nandan2@gmail.com>
2020-07-03 19:05:05 +0530

    Added next and previous layout

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from tkinter import *
 from win import Win
-from footer import Footer
 
 
-class App(Tk):
-
+class App:
     def __init__(self, title=None):
+        self.root = Tk()
         window_title = 'TheNandan'
         if title is not None:
-            window_title = window_title + ' - ' + title
-        super().__init__(className=window_title)
+            self.root.title = window_title + ' - ' + title
+        self.root.title(window_title)
         self.window = Win(self)
-        self.footer = Footer(self)
+
+    def switchWindow(self, window):
+        self.window.destroy()
+        self.window = window(self)
 
 
 if __name__ == "__main__":
     app = App()
-    app.mainloop()
+    app.root.mainloop()
--- footer.py
+++ footer.py
@@ -2,9 +2,23 @@ from tkinter import Canvas, Button, X, BOTTOM
 
 
 class Footer:
-
-    def __init__(self, master):
-        self.canvas = Canvas(master, bg="white", height=60)
+    def __init__(self, app, nextWin=None, prevWin=None):
+        self.app = app
+        self.nextWin = nextWin
+        self.prevWin = prevWin
+        self.canvas = Canvas(app.root, bg="white", height=60)
         self.canvas.pack(fill=X, side=BOTTOM)
-        self.next_btn = Button(master, text="Next")
-        self.next_btn.place(relx=0.9, rely=0.9)
+        if nextWin:
+            self.next_btn = Button(app.root, text="Next", command=self.nextWindow)
+            self.next_btn.place(relx=0.9, rely=0.9)
+        if prevWin:
+            self.next_btn = Button(app.root, text="Prev", command=self.prevWindow)
+            self.next_btn.place(relx=0.9, rely=0.9)
+
+    def nextWindow(self):
+        if self.nextWin:
+            self.app.switchWindow(self.nextWin)
+
+    def prevWindow(self):
+        if self.prevWin:
+            self.app.switchWindow(self.prevWin)
