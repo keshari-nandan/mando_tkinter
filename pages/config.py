ece2b0eec1fb4cbea5518ab0c59aeb306d8ad325
Keshari Nandan <keshari.nandan2@gmail.com>
2020-07-05 10:21:44 +0530

    Add venv

--- pages/config.py
+++ pages/config.py
@@ -13,30 +13,27 @@ class Config(Base):
         self.databases = ["MySQL", "MariaDB", "PostgreSQL", "MongoDB"]
         super().__init__(app, "Configuration")
 
-
-
         # Programming Language Selection
         lang_label = Label(self.top_frame, text='Programming Language: ', padx=20)
         lang_label.grid(row=0, sticky="e")
         self.default_lang = StringVar(self.top_frame)
-        self.default_lang.set(self.languages[0]) # default value
+        self.default_lang.set(self.languages[0])  # default value
         self.lang_val = OptionMenu(self.top_frame, self.default_lang, *self.languages, command=self.langEvent)
         self.lang_val.configure(width=10)
         self.lang_val.grid(row=0, column=1, sticky="w")
 
         # Programming Language versions Selection
         self.default_version = StringVar(self.top_frame)
-        self.default_version.set(self.versions[0]) # default value
+        self.default_version.set(self.versions[0])  # default value
         self.version_val = OptionMenu(self.top_frame, self.default_version, *self.versions)
         self.version_val.configure(width=10)
         self.version_val.grid(row=0, column=3, sticky="w")
-
 
         # Framework Selection
         framework_label = Label(self.top_frame, text='Framework: ', padx=20)
         framework_label.grid(row=2, sticky="e")
         self.default_framework = StringVar(self.top_frame)
-        self.default_framework.set(self.frameworks[0]) # default value
+        self.default_framework.set(self.frameworks[0])  # default value
         self.framework_val = OptionMenu(self.top_frame, self.default_framework, *self.frameworks)
         self.framework_val.configure(width=10)
         self.framework_val.grid(row=2, column=1, sticky="w")
@@ -45,7 +42,7 @@ class Config(Base):
         server_label = Label(self.top_frame, text='Web Server: ', padx=20)
         server_label.grid(row=3, sticky="e")
         self.default_server = StringVar(self.top_frame)
-        self.default_server.set(self.servers[0]) # default value
+        self.default_server.set(self.servers[0])  # default value
         self.server_val = OptionMenu(self.top_frame, self.default_server, *self.servers)
         self.server_val.configure(width=10)
         self.server_val.grid(row=3, column=1, sticky="w")
@@ -54,11 +51,10 @@ class Config(Base):
         db_label = Label(self.top_frame, text='Database : ', padx=20)
         db_label.grid(row=4, sticky="e")
         self.default_db = StringVar(self.top_frame)
-        self.default_db.set(self.databases[0]) # default value
+        self.default_db.set(self.databases[0])  # default value
         self.db_val = OptionMenu(self.top_frame, self.default_db, *self.databases)
         self.db_val.configure(width=10)
         self.db_val.grid(row=4, column=1, sticky="w")
-
 
         # Redis Configuration
         redis_label = Label(self.top_frame, text='Redis: ', padx=20)
@@ -67,7 +63,6 @@ class Config(Base):
         self.redis_val.set(False)
         self.redis_opt = Checkbutton(self.top_frame, var=self.redis_val)
         self.redis_opt.grid(row=5, column=1, sticky="w")
-
 
         # Message Broker
         broker_label = Label(self.top_frame, text='Message Broker: ', padx=20)
@@ -82,20 +77,20 @@ class Config(Base):
         self.rabbitmq_opt.grid(row=6, column=3, sticky="w")
 
         # Submit Buttom
-        submit_btn = Button(self.center, text="Next", fg="white", bg="RoyalBlue1", activebackground="RoyalBlue2", relief="flat", width="8", command=self.nextClicked)
+        submit_btn = Button(self.center, text="Next", fg="white", bg="RoyalBlue1", activebackground="RoyalBlue2",
+                            relief="flat", width="8", command=self.nextClicked)
         submit_btn.grid(row=7, column=3, sticky="e")
-
 
     def nextClicked(self):
         config = {
-            "langurage" : self.default_lang.get(),
-            "lang_version" : self.default_version.get(),
-            "framework" : self.default_framework.get(),
-            "server" : self.default_server.get(),
-            "database" : self.default_db.get(),
-            "redis" : self.redis_val.get(),
-            "activemq" : self.activemq_val.get(),
-            "rabbitmq" : self.rabbitmq_val.get()
+            "language": self.default_lang.get(),
+            "lang_version": self.default_version.get(),
+            "framework": self.default_framework.get(),
+            "server": self.default_server.get(),
+            "database": self.default_db.get(),
+            "redis": self.redis_val.get(),
+            "activemq": self.activemq_val.get(),
+            "rabbitmq": self.rabbitmq_val.get()
         }
         file = open('settings/system.json', 'w')
         json.dump(config, file)
@@ -148,7 +143,6 @@ class Config(Base):
 
         # lang_val = Entry(self.top_frame)
         # lang_val.grid(row=0, column=1)
-
 
         # width_label = Label(self.top_frame, text='Width:')
         # length_label = Label(self.top_frame, text='Length:')
